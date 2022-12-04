711a1f3dbf95f90204013af45b00ebb0a3b53c93
Jorge Mimoso <jorge.mimoso@syone.com>
2022-12-04 23:12:55 +0000

    - projeto file added;

---
+++ projeto.lisp
@@ -0,0 +1,3 @@
+;;;; Ficheiro central do Projeto - Trata da interação com o utilizador, carregando e lendo os ficheiros necessários
+;;;; Projeto IA Fase 1 - Dots and Boxes
+;;;; Autores: Guilherme Martins 201802243, Jorge Mimoso 202000695
