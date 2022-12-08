dcfe908afa49ad1bc3f9b6aab99c5aa66a0ab114
Jorge Mimoso <jorge.mimoso@syone.com>
2022-12-08 22:39:53 +0000

    - tabuleiro teste lab 7 adicionado; - método no-solucaop adicionado; - lista de operadores adicionado; - método de novo sucessor adicionado;

--- procura.lisp
+++ procura.lisp
@@ -4,6 +4,23 @@
 
 
 ;;###################################AUXILIARES#############################################
+(defun tabuleiro-teste ()
+  "Retorna um tabuleiro 3x3 (3 arcos na vertical por 3 arcos na horizontal)"
+	'(
+		((0 0 0) (0 0 1) (0 1 1) (0 0 1))
+		((0 0 0) (0 1 1) (1 0 1) (0 1 1))
+	)
+)
+
+(defun no-tabuleiro-teste ()
+     '(
+       (
+        ((1 1 1) (1 0 1) (0 1 1) (0 0 1)) 
+        ((1 0 0) (0 1 1) (1 0 1) (0 1 1))
+       ) 0 nil)
+)
+
+
 
 (defun no-teste ()
      '(
@@ -100,7 +117,7 @@
      )
 )
 
-;;esta a retornar 1+ do que suposto
+;;Fixed -> actualizei apenas para verificar numa direção (horizontal), se verificarmos as duas direções, conta caixas repetidas
 (defun num-caixas-fechadas (no &optional (line 1) (col 1)) 
      (cond
          ((or 
@@ -112,17 +129,70 @@
                (= col (length (get-arco-horizontal (no-estado no)))) 
                (= col (length (get-arco-vertical (no-estado no)))) 
              )
-             (not (caixa-fechadap no line col))
+             (not (caixa-fechada-hrzp no line col))
           )(+ 0 (num-caixas-fechadas no (1+ line) 1)))
          ((and 
              (or 
                (= col (length (get-arco-horizontal (no-estado no)))) 
                (= col (length (get-arco-vertical (no-estado no)))) 
              )
-             (caixa-fechadap no line col)
+             (caixa-fechada-hrzp no line col)
           )(+ 1 (num-caixas-fechadas no (1+ line) 1)))
-         ((caixa-fechadap no line col) (+ 1 (num-caixas-fechadas no line (1+ col))))
+         ((caixa-fechada-hrzp no line col) (+ 1 (num-caixas-fechadas no line (1+ col))))
          (t (+ 0 (num-caixas-fechadas no line (1+ col))))
      )
 )
+
+
+;;; predicado no-solucaop que verifica se um estado e final
+;; teste: (no-solucaop (no-teste) nCaixasFechadas)
+;; resultado: NIL
+(defun no-solucaop(no &optional (solucao 1))
+  (cond
+    ((= solucao (num-caixas-fechadas  no ))t)
+  )
+)
+
+
+(defun operadores ()
+ "Cria uma lista com  os operadores do problema."
+ (list 'arco-horizontal 'arco-vertical))
+
+
+
+(defun novo-sucessor(no op &optional (line 1)(col 1))
+   
+  (cond
+    ((equal op 'arco-horizontal) 
+      (cond
+          
+       ((= (length (car (car(no-estado no )))) col ) (novo-sucessor no 'arco-horizontal (+ 1 line) 1)) 
+       ((= (length (car (no-estado no))  ) line) nil)       
+       ((equal (arco-horizontal line col (no-estado no))  nil) (novo-sucessor no 'arco-horizontal line (+ 1 col)))         
+       (t  (criar-no  (arco-horizontal line col (no-estado no)) (+ (no-profundidade no) 1) (no-estado no)))
+                   
+       )
+    )
+     ((equal op 'arco-vertical) 
+      (cond
+          
+       ((= (length (car (car(no-estado no )))) col ) (novo-sucessor no 'arco-vertical (+ 1 line) 1)) 
+       ((= (length (car (no-estado no))  ) line) nil)       
+       ((equal (arco-vertical line col (no-estado no))  nil) (novo-sucessor no 'arco-vertical line (+ 1 col)))         
+       (t  (criar-no  (arco-vertical line col (no-estado no)) (+ (no-profundidade no) 1) (no-estado no)))
+                   
+       )
+    )
+)
+)
+   
+
+
+
+
+
+
+;; se nil, coloca no seguinte
+
+;; verificar se indices excedem o array
 
