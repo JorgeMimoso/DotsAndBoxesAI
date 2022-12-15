dc692ce34c82901fb354573a30cce94e478bf7b4
Jorge Espiguinha Mimoso <jemimoso@gmail.com>
2022-12-15 21:58:33 +0000

    Add files via upload

--- procura.lisp
+++ procura.lisp
@@ -20,6 +20,8 @@
        ) 0 nil)
 )
 
+
+
 (defun no-teste ()
      '(
        (
@@ -27,6 +29,16 @@
         ((0 0 0 0) (0 0 0 0) (0 0 0 1) (0 0 0 1))
        ) 0 nil)
 )
+
+(defun no-teste-solucao ()
+     '(
+       (
+        ((1 1 1) (1 1 1) (1 1 1) (1 1 1) (1 1 1)) 
+        ((1 1 1 1) (1 1 1 1) (1 1 1 1) (1 1 1 1))
+       ) 0 nil)
+)
+
+
 
 (defun no-teste2 ()
      '(
@@ -154,13 +166,11 @@
      )
 )
 
-
-;;; predicado no-solucaop que verifica se um estado e final
-;; teste: (no-solucaop (no-teste) nCaixasFechadas)
-;; resultado: NIL
-(defun no-solucaop(no &optional (solucao 1))
+(defun no-solucaop(no &optional(solucao 13 ))
   (cond
-    ((= solucao (num-caixas-fechadas no)) t)
+    ((equal (no-estado no) (no-estado(no-teste-solucao)) )t)
+        ;((<= solucao (num-caixas-fechadas  no ))t)
+   (t nil)
   )
 )
 
@@ -175,16 +185,16 @@
   (cond
     ((equal op 'arco-horizontal) 
       (cond        
-       ((= (length (car (car (no-estado no)))) col) (novo-sucessor no 'arco-horizontal (+ 1 line) 1)) 
-       ((= (length (car (no-estado no))) line) nil)       
+       ((< (length (car (car(no-estado no )))) col ) (novo-sucessor no 'arco-horizontal (+ 1 line) 1)) 
+       ((< (length (car (no-estado no))  ) line) nil)       
        ((equal (arco-horizontal line col (no-estado no))  nil) (novo-sucessor no 'arco-horizontal line (+ 1 col)))         
        (t  (criar-no  (arco-horizontal line col (no-estado no)) (+ (no-profundidade no) 1) (no-estado no)))               
        )
     )
      ((equal op 'arco-vertical) 
       (cond          
-       ((= (length (car (car(no-estado no )))) line ) (novo-sucessor no 'arco-vertical 1 (+ 1 col))) 
-       ((= (length (car (no-estado no))  ) col) nil)       
+       ((< (length (car (car(no-estado no )))) line ) (novo-sucessor no 'arco-vertical 1 (+ 1 col))) 
+       ((< (length (car (no-estado no))  ) col) nil)       
        ((equal (arco-vertical line col (no-estado no))  nil) (novo-sucessor no 'arco-vertical (+ 1 line)col ))         
        (t  (criar-no  (arco-vertical line col (no-estado no)) (+ (no-profundidade no) 1) (no-estado no)))                  
        )
@@ -199,26 +209,47 @@
   )
 )
 
-(defun abertos-bfs (abertos nos-sucessores)
-     (append abertos nos-sucessores)
+
+
+
+
+
+
+(defun abertos-bfs (abertos sucessores)
+  (append abertos sucessores)
 )
 
-(defun abertos-dfs (abertos nos-sucessores)
-     (append nos-sucessores abertos)
+(defun abertos-dfs (abertos sucessores)
+  (append sucessores abertos)
 )
 
-(defun no-existep (no no-list algoritmo)
+
+;;Implementacao Lab 6
+(defun no-existep (no lista)
+   (not (null (find (no-estado no) (mapcar #'no-estado lista))))
+)
+
+(defun no-existep2 (no no-list &optional algoritmo)
      (cond
          ((null no-list) nil)
          ((and (equal (no-estado no) (no-estado (first no-list)))) (eq algoritmo 'bfs) t)
          ((and (equal (no-estado no) (no-estado (first no-list))) (= (no-profundidade (first no-list)))) t)
-         (t (no-existep no (rest no-list) algoritmo))
+         (t (no-existep2 no (rest no-list) algoritmo))
      )
 )
-  
 
 
 
-
-
-
+;; implementacao lab 6
+(defun bfs (no-inicial solucaop sucessores operadores &optional abertos fechados)
+  (let* (
+         (abertos (abertos-bfs abertos 
+                       (remove nil (mapcar (lambda(x) (cond ((or (no-existep x fechados) (no-existep x abertos)) NIL) (T x))) (sucessores no-inicial operadores 'bfs)))
+                  )
+         )
+         (fechados (cons no-inicial fechados))
+        )
+  (cond ((remove nil (mapcar solucaop abertos)) (find-if solucaop abertos))
+        (T (bfs (car abertos) solucaop sucessores operadores (cdr abertos) fechados))
+  ))
+)
